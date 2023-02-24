570ab8bec0851fc3eea34191c0159f68acc6bb64
Shriyansh2410 <126236630+Shriyansh2410@users.noreply.github.com>
2023-02-24 13:59:07 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,470 @@
+
+var quiz = { "JS" : [
+	{
+		"id" : 1,
+		"question" : "Who is the prime minister of India?",
+		"options" : [
+			{"a": "Mr. Narendra Modi", 
+			 "b":"Mr.R. Gandhi", 
+			 "c":"Mr.Yaswant Sinha", 
+			 "d":"Mr.Ramnath Covind"}
+			],
+		"answer":"Mr. Narendra Modi",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 2,
+		"question" : "Shri Ram was the avtar of which Hindu God?",
+		"options" : [
+			{"a": "Lord Mahadev Ji", 
+			 "b":"Lord Brahma Ji", 
+			 "c":"Lord Vishnu Ji",
+                   "d":"Lord Surya Ji"}
+			],
+		"answer":"Lord Vishnu Ji",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 3,
+		"question" : "Which one of the river flows between Vindhyan and Satputra Ranges?",
+		"options" : [
+			{"a": "Netravati", 
+			 "b":"Son", 
+			 "c":"Narmada"}
+			],
+		"answer":"Narmada",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 4,
+		"question" : "The Central Rice Research Station is situated in?",
+		"options" : [
+			{"a": "Cuttak", 
+			 "b":"Chennai",
+                   "c":"Banglore",
+			 "d":"Quilon"}
+			],
+		"answer":"Cuttak",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 5,
+		"question" : "Who among the following wrote Sanskrit grammer?",
+		"options" : [
+			{"a": "Aryabhatt", 
+			 "b":"Charak",
+			 "c":"Kalidasa",
+			 "d":"Panini"}
+			],
+		"answer":"Panini",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 6,
+		"question" : "What country has the heighest life expectancy?",
+		"options" : [
+			{"a": "Hong Kong", 
+			 "b":"India",
+			 "c":"Japan",
+			 "d":"USA"}
+			],
+		"answer":"Hong Kong",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 7,
+		"question" : "What year was the United Nations establieshed?",
+		"options" : [
+			{"a": "1940", 
+			 "b":"1995",
+			 "c":"1945",
+			 "d":"1955"}
+			],
+		"answer":"1945",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 8,
+		"question" : "How to write an IF statement in JavaScript?",
+		"options" : [
+			{"a": "if i = 5 then", 
+			 "b":"if i == 5 then",
+			 "c":"if (i == 5)",
+			 "d":" if i = 5",
+			}
+			],
+		"answer":"if (i == 5)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 9,
+		"question" : "How many elements are in periodic table?",
+		"options" : [
+			{"a": "108", 
+			 "b":"118",
+			 "c":"218",
+			 "d":"120"
+			}
+			],
+		"answer":"118",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 10,
+		"question" : "Development expenditure of the Central government does not include?",
+		"options" : [
+			{"a": "Defence expenditure", 
+			 "b":"Expenditure on social and community services",
+			 "c":"Grant to states",
+			 "d":"Expenditure on economic services",
+			}
+			],
+		"answer":"Defence expenditure",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 11,
+		"question" : "How does a WHILE loop start?",
+		"options" : [
+			{"a": "while (i &lt;=10);", 
+			 "b":"while(i &lt;= 10;i++)",
+			 "c":"while (i &lt;= 10)",
+                   "d":"Non of the Above",
+			}
+			],
+		"answer":"while (i &lt;= 10)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 12,
+		"question" : "Which of the following is a disadvantage of using JavaScript?",
+		"options" : [
+			{"a": "Client-side JavaScript does not allow the reading or writing of files.", 
+			 "b":"JavaScript can not be used for Networking applications because there is no such support available.",
+			 "c":"JavaScript doesn't have any multithreading or multiprocess capabilities.",
+			 "d":"All of the above."
+			}
+			],
+		"answer":"All of the above.",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 13,
+		"question" : "What percentage of irrigated land in the India?",
+		"options" : [
+			{"a": "35%", 
+			 "b":"45%",
+			 "c":"65%",
+                   "d":"75%",
+			}
+			],
+		"answer":"35%",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 14,
+		"question" : "How to insert a comment that has more than one line?",
+		"options" : [
+			{"a": "/*This comment has more than one line*/", 
+			 "b":"//This comment has more than one line//",
+			 "c":"&lt;!--This comment has more than one line--&gt;"
+			}
+			],
+		"answer":"/*This comment has more than one line*/",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 15,
+		"question" : "By Whom and When C Language was developed?",
+		"options" : [
+			{"a": "Tim Berners-Lee in 1993", 
+			 "b":"James Gosling OC in 1995",
+			 "c":"Dennis Ritchie in 1972",
+			 "d":"Non of the Above",
+			}
+			],
+		"answer":"Dennis Ritchie in 1972",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 16,
+		"question" : "How do you round the number 7.25, to the nearest integer?",
+		"options" : [
+			{"a": "rnd(7.25)", 
+			 "b":"Math.round(7.25)",
+			 "c":"Math.rnd(7.25)",
+			 "d":"round(7.25)"
+			}
+			],
+		"answer":"Math.round(7.25)",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 17,
+		"question" : "Among these languages which language is not case sensitive?",
+		"options" : [
+			{"a": "Java", 
+			 "b":"C",
+			 "c":"Html",
+			 "d":"C++"
+			}
+			],
+		"answer":"Html",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 18,
+		"question" : "What is the extension of Html file?",
+		"options" : [
+			{"a": ".htm", 
+			 "b":".htl",
+			}
+			],
+		"answer":".htm",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 19,
+		"question" : "JavaScript is the same as Java.",
+		"options" : [
+			{"a": "true", 
+			 "b":"false"
+
+			}
+			],
+		"answer":"false",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 20,
+		"question" : "Among these which is the largest state of India?",
+		"options" : [
+			{"a": "Madhya Pradesh", 
+			 "b":"Rajasthan",
+			 "c":"Uttar Pradesh"
+			}
+			],
+		"answer":"Rajasthan",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 21,
+		"question" : "What is the new target set for Defence Exports in 2022-23?",
+		"options" : [
+			{"a": "Rs 15000 crore", 
+			 "b":"Rs 20000 crore",
+			 "c":"Rs 40000 crore",
+			 "d":"Rs 50000 crore"
+			}
+			],
+		"answer":"Rs 40000 crore",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 22,
+		"question" : "How do you declare a JavaScript variable?",
+		"options" : [
+			{"a": "var carName;", 
+			 "b":"variable carName;",
+			 "c":"v carName;"
+			}
+			],
+		"answer":"var carName;",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 23,
+		"question" : "Which operator is used to assign a value to a variable?",
+		"options" : [
+			{"a": "*", 
+			 "b":"-",
+			 "c":"=",
+			 "d":"x"
+			}
+			],
+		"answer":"=",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 24,
+		"question" : "What will the following code return: Boolean(10 &gt; 9)",
+		"options" : [
+			{"a": "NaN", 
+			 "b":"false",
+			 "c":"true"
+			}
+			],
+		"answer":"true",
+		"score":0,
+		"status": ""
+	},
+	{
+		"id" : 25,
+		"question" : "Is JavaScript case-sensitive?",
+		"options" : [
+			{"a": "No", 
+			 "b":"Yes"
+			}
+			],
+		"answer":"Yes",
+		"score":0,
+		"status": ""
+	}
+	]
+}
+
+
+
+var quizApp = function() {
+
+	this.score = 0;		
+	this.qno = 1;
+	this.currentque = 0;
+	var totalque = quiz.JS.length;
+
+	
+	this.displayQuiz = function(cque) {
+		this.currentque = cque;
+		if(this.currentque <  totalque) {
+			$("#tque").html(totalque);
+			$("#previous").attr("disabled", false);
+			$("#next").attr("disabled", false);
+			$("#qid").html(quiz.JS[this.currentque].id + '.');
+	
+			
+			$("#question").html(quiz.JS[this.currentque].question);	
+			 $("#question-options").html("");
+			for (var key in quiz.JS[this.currentque].options[0]) {
+			  if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+		
+				$("#question-options").append(
+					"<div class='form-check option-block'>" +
+					"<label class='form-check-label'>" +
+							  "<input type='radio' class='form-check-input' name='option'   id='q"+key+"' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+								  quiz.JS[this.currentque].options[0][key] +
+							 "</span></label>"
+				);
+			  }
+			}
+		}
+		if(this.currentque <= 0) {
+			$("#previous").attr("disabled", true);	
+		}
+		if(this.currentque >= totalque) {
+				$('#next').attr('disabled', true);
+				for(var i = 0; i < totalque; i++) {
+					this.score = this.score + quiz.JS[i].score;
+				}
+			return this.showResult(this.score);	
+		}
+	}
+	
+	this.showResult = function(scr) {
+		$("#result").addClass('result');
+		$("#result").html("<h1 class='res-header'>Total Score: &nbsp;" + scr  + '/' + totalque + "</h1>");
+		for(var j = 0; j < totalque; j++) {
+			var res;
+			if(quiz.JS[j].score == 0) {
+					res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+			} else {
+				res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+			}
+			$("#result").append(
+			'<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+			'<div><b>Correct answer:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+			'<div class="last-row"><b>Score:</b> &nbsp;' + res +
+			
+			'</div>' 
+			
+			);
+			
+		}
+	}
+	
+	this.checkAnswer = function(option) {
+		var answer = quiz.JS[this.currentque].answer;
+		option = option.replace(/\</g,"&lt;")   //for <
+		option = option.replace(/\>/g,"&gt;")   //for >
+		option = option.replace(/"/g, "&quot;")
+
+		if(option ==  quiz.JS[this.currentque].answer) {
+			if(quiz.JS[this.currentque].score == "") {
+				quiz.JS[this.currentque].score = 1;
+				quiz.JS[this.currentque].status = "correct";
+		}
+		} else {
+			quiz.JS[this.currentque].status = "wrong";
+		}
+		
+	}	
+	 
+	this.changeQuestion = function(cque) {
+			this.currentque = this.currentque + cque;
+			this.displayQuiz(this.currentque);	
+			
+	}
+	
+}
+
+
+var jsq = new quizApp();
+
+var selectedopt;
+	$(document).ready(function() {
+			jsq.displayQuiz(0);		
+		
+	$('#question-options').on('change', 'input[type=radio][name=option]', function(e) {
+
+			//var radio = $(this).find('input:radio');
+			$(this).prop("checked", true);
+				selectedopt = $(this).val();
+		});
+		
+			
+			 
+	});
+
+	
+	
+	
+	$('#next').click(function(e) {
+			e.preventDefault();
+			if(selectedopt) {
+				jsq.checkAnswer(selectedopt);
+			}
+			jsq.changeQuestion(1);
+	});	
+	
+	$('#previous').click(function(e) {
+		e.preventDefault();
+		if(selectedopt) {
+			jsq.checkAnswer(selectedopt);
+		}
+			jsq.changeQuestion(-1);
+	});	
+
+
+
